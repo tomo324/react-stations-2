c6abf913878cadbe61beee50a2b54f4068ba49e3
tomo324 <0204tomo.mc@gmail.com>
2024-08-27 23:43:07 +0900

    fix: improve style in NotFound Page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./NotFound.css";
 
 export const NotFound = () => {
   return (
-    <>
+    <div className="container">
       <h1>404</h1>
       <h3>お探しのページは見つかりませんでした。</h3>
-    </>
+    </div>
   );
-}
+};
